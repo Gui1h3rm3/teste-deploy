a81613e182c91e45aedecd2848204a71c3a98579
Guilherme <guilhermebha1989@gmail.com>
2025-10-22 23:55:24 -0300

    Commit inicial

---
+++ src/main/java/com/guilherme/deploy_backend/DeployBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.deploy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/deploy_backend/DeployBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.deploy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
